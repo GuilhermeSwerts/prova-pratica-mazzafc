eaa5d2eb942bf16f8f505402de2e401083037062
ViveoPortais <viveo.portais@gmail.com>
2025-05-10 17:17:52 -0300

    fix: Projetos que estavam faltando

---
+++ prova-pratica-mazzafc.Ioc/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace prova_pratica_mazzafc.Ioc
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection RegisterServices(this IServiceCollection services)
+        {
+            return services;
+        }
+
+        public static IServiceCollection RegisterRepositories(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
